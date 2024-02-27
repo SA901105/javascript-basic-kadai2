501676efba76ccdae15e36d65a1ebca8c7d995d2
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-02-27 17:23:59 +0900

    初回コミット

---
+++ bmi.js
@@ -0,0 +1,15 @@
+//体重
+let Height;
+Height = 1.7;
+
+//身長
+let Weight;
+Weight = 68;
+
+//BMI算出公式
+console.log(Weight / (Height * Height));
+
+//コンソールへの出力
+console.log(Height);
+console.log(Weight);
+

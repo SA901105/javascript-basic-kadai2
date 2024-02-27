87245a3e88ab6928357b597e2d9fd96ff1191d28
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-02-27 22:46:25 +0900

    BMIを変数として代入

--- bmi.js
+++ bmi.js
@@ -6,10 +6,12 @@ Height = 1.7;
 let Weight;
 Weight = 68;
 
-//BMI算出公式
-console.log(Weight / (Height * Height));
+//BMI
+let Bmi;
+Bmi = (Weight / (Height * Height)) ;
 
 //コンソールへの出力
 console.log(Height);
 console.log(Weight);
+console.log(Bmi);
 

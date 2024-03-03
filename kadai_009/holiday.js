1358b3ab39a8698cb4bb9aae441b2bfa688f37be
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-03-03 16:33:07 +0900

    課題009を追加

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,18 +1,18 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 console.log(holidays);
 
-let holidays = Math.floor(Math.random() * 16);
+let i = 0
 
-while(holidays !== 0){
-  holidays = Math.floor(Math.random() * 16)
+while (i < 16){
+  console.log(holidays[i]);
+  i++
 }
+
 
 
 // 0～15までの数値を順番に出力する
-for  (let i = 1; i = 16; i++){
-  console.log(holidays);
+for (let i = 0; i < 16; i++){
+  console.log(holidays[i]);
 }
 
 
-
-while

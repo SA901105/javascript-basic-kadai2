c0055c4bfc3dc38789a262ad88694493686e2517
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-03-05 16:52:59 +0900

    課題021tuika

---
+++ kadai_021/event.js
@@ -0,0 +1,24 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// btnがクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+
+  // h2要素を新しく作成する
+  const childText = document.createElement('h2');
+  
+  // 2秒後にh2要素を「ボタンをクリックしました」というテキストに書き換える
+  setTimeout(() => {
+
+    //作成したh2要素に「ボタンをクリックしました」というテキストを追加する
+    childText.textContent = 'ボタンをクリックしました';
+
+    // 作成したh2要素を書き換える
+    text.textContent = 'ボタンをクリックしました';
+  
+  }, 2000);
+
+});

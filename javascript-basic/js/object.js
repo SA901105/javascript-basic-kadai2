5ab24c962c13ea843639af0fabee84dae88aba86
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-03-08 21:29:38 +0900

    javascript-basicを追加

---
+++ javascript-basic/js/object.js
@@ -0,0 +1,12 @@
+// オブジェクトの宣言と値の代入を行う
+const personalDate = {name: '侍太郎', age: 36, gender: '男性'};
+
+// オブジェクトの値を出力する
+console.log(personalDate);
+
+// 'age'というキーの値を更新する
+personalDate.address = '東京都';
+
+console.log(personalDate);
+
+console.log(personalDate.gender);
---
+++ javascript-basic/js/variable.js
@@ -0,0 +1,20 @@
+//変数の宣言
+let userName;
+
+//値の代入
+userName = '侍太郎';
+
+//変数の宣言・値の代入
+let userNamber = 55;
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNamber);
+
+//値の再代入
+userName = '侍花子'
+userNamber = 88;
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNamber);
